08eebe71eed48f89ea22e214e7e4327d6cbe66df
KaniRobinson <me@kanirobinson.co.uk>
2020-09-13 20:02:08 +0100

    Initial commit

---
+++ src/Aggregator.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Aggregators\OhSeeMedia;
+
+use Carbon\Carbon;
+use InvalidArgumentException;
+use Aggregators\Support\BaseAggregator;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Aggregator extends BaseAggregator
+{
+    /**
+     * {@inheritDoc}
+     */
+    public string $uri = 'https://ohseemedia.com/categories/';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $provider = 'Oh see media';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $logo = 'logo.png';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function articleIdentifier(): string
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function nextUrl(Crawler $crawler): ?string
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function image(Crawler $crawler): ?string
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function title(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('h3')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function content(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('div:last-of-type')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function link(Crawler $crawler): ?string
+    {
+        try {
+            return 'https://ohseemedia.com' . $crawler->filter('h3 > a')->attr('href');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateCreated(Crawler $crawler): Carbon
+    {
+        try {
+            return Carbon::parse($crawler->filter('time')->attr('datetime'));
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateUpdated(Crawler $crawler): Carbon
+    {
+        return $this->dateCreated($crawler);
+    }
+}
